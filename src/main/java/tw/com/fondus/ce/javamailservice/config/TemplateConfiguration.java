6d14e725305b780a08715c85c8bafb197239aae9
yue0910 <la92142850910@gmail.com>
2021-11-30 18:46:52 +0800

    add TemplateConfiguration

---
+++ src/main/java/tw/com/fondus/ce/javamailservice/config/TemplateConfiguration.java
@@ -0,0 +1,15 @@
+package tw.com.fondus.ce.javamailservice.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties( value = "templates")
+@Data
+@NoArgsConstructor
+public class TemplateConfiguration {
+	String name;
+	String folder;
+}
