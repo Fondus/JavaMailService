30172d92f2c29ccfd695fc27ca14b708d570ad9d
yue0910 <la92142850910@gmail.com>
2021-11-30 18:48:48 +0800

    add templateVersion to template path

--- src/main/java/tw/com/fondus/ce/javamailservice/service/impl/FreeMarkerContentServiceImpl.java
+++ src/main/java/tw/com/fondus/ce/javamailservice/service/impl/FreeMarkerContentServiceImpl.java
@@ -1,5 +1,6 @@
 package tw.com.fondus.ce.javamailservice.service.impl;
 
+import com.jayway.jsonpath.JsonPath;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -7,6 +8,7 @@ import freemarker.template.TemplateNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tw.com.fondus.ce.javamailservice.config.TemplateConfiguration;
 import tw.com.fondus.ce.javamailservice.service.ContentService;
 import tw.com.fondus.ce.javamailservice.service.exception.ContentGenerationException;
 import tw.com.fondus.ce.javamailservice.vo.Content;
@@ -15,19 +17,31 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Map;
 
-@Service @Slf4j public class FreeMarkerContentServiceImpl implements ContentService {
-	@Autowired Configuration configuration;
+@Service
+@Slf4j
+public class FreeMarkerContentServiceImpl implements ContentService {
+	@Autowired
+	Configuration configuration;
+	@Autowired
+	TemplateConfiguration templateConfiguration;
 
-	@Override public Content generate( String template, Map<String, Object> data ) throws ContentGenerationException {
+	@Override
+	public Content generate( String template, Map<String, Object> data ) throws ContentGenerationException {
 		try {
 			String subject = process( template + "/subject.ftl", data );
 			if ( subject == null )
 				subject = template;
-			String text = process( template + "/text.ftl", data );
-			String html = process( template + "/html.ftl", data );
+			String templateVersion = JsonPath.parse( data.toString() )
+					.read( templateConfiguration.getFolder() );
+			String text = process( template + "/" + templateVersion + "/text.ftl", data );
+			String html = process( template + "/" + templateVersion + "/html.ftl", data );
 			if ( text == null && html == null )
 				throw new ContentGenerationException( "template not found" );
-			return Content.builder().subject( subject ).text( text ).html( html ).build();
+			return Content.builder()
+					.subject( subject )
+					.text( text )
+					.html( html )
+					.build();
 		} catch (IOException | TemplateException e) {
 			log.error( "Fail to generate html content using template " + template, e );
 			throw new ContentGenerationException( "IOException", e );
