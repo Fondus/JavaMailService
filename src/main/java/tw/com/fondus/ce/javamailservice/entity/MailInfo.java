7175bfd9c7b0ad9d33dd8097fdf1cb39ae000b1f
Bread God <Vipcube@users.noreply.github.com>
2021-12-11 13:07:49 +0800

    Fixed the compile error.

--- src/main/java/tw/com/fondus/ce/javamailservice/entity/MailInfo.java
+++ src/main/java/tw/com/fondus/ce/javamailservice/entity/MailInfo.java
@@ -1,5 +1,6 @@
 package tw.com.fondus.ce.javamailservice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class MailInfo {
 	@Builder.Default
